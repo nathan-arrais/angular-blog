95a6a060f74d569e7a1d33d9ab0967e8edd74f21
Nathan Arrais <nathan.arrais01@gmail.com>
2024-02-25 22:07:16 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://conteudo.imguol.com.br/c/entretenimento/48/2022/03/25/cenario-de-filme-western-na-andaluzia-espanha-1648225194403_v2_900x506.jpg",
+    "title": "Lorem Ipsum",
+    "description": "bla bla bla"
+  },
+  {
+    "id": "2",
+    "photo": "https://seriesnews.com.br/wp-content/uploads/2023/10/1883-serie-390x220.webp",
+    "title": "Lorem Ipsum",
+    "description": "ola"
+  },
+  {
+    "id": "3",
+    "photo": "https://www.planocritico.com/wp-content/uploads/2014/08/Plano-Cr%C3%ADtico-Especial-Western-Faroeste.jpg",
+    "title": "Dolor sit",
+    "description": "bla bla bla"
+  },
+  {
+    "id": "4",
+    "photo": "https://st3.idealista.pt/news/arquivos/styles/fullwidth_xl/public/2018-03/pioneertown_california_saloon_and_bath_house.jpg?VersionId=LP4equW95lAzZxvjzfSfVnTYboZccTmN&itok=YrpLJV_C",
+    "title": "Amet consectetur",
+    "description": "bla bla bla"
+  }
+]
